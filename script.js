216e7778030483907dbc16ddb64e8fae98eafde1
alexnoy <alexander.noy123@gmail.com>
2025-09-29 20:16:07 -0400

    Create gameboard function
    The function controls the creation of the board, it provides methods to get the board, mark a square on the board, and to show the board with the values on them.

---
+++ script.js
@@ -0,0 +1,35 @@
+const gameboard = (function() {
+    const board = [];
+
+    for (let i = 0; i < 3; i++) {
+        board[i] = [];
+        for (let j = 0; j < 3; j++) {
+            board[i].push(square());
+        }
+    } 
+
+    const getBoard = () => board;
+
+    const markSquare = (row, column, player) => {    
+        const selectedSquare = board[row][column];
+
+        if (selectedSquare.getValue() !== 0) {
+            console.log(new Error('Invalid input select another square'));
+        } else {
+            selectedSquare.setSquare(player)
+        }
+
+        showBoard();
+    }
+
+    const showBoard = () => {
+        const boardValues =  board.map((row) => row.map((square) => square.getValue()))
+        console.table(boardValues);
+    }
+
+    return {
+        getBoard,
+        showBoard,
+        markSquare
+    }
+})();
